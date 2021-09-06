2292a92e97b0931e497e8307ea7dc5ccd182319a
Faruque Braimo <faruquebraimo@gmail.com>
2021-09-06 17:03:56 +0200

    ,

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
 import Fuse from 'fuse.js';
+import Highlight from 'react-highlighter';
+
 
 import characters from './characters.json';
 
@@ -37,16 +39,20 @@ function App() {
           {characterResults.map(character => {
             const { name, company, species, thumb } = character;
             return (
+              
               <li key={name} className="character">
+              
+
                 <span className="character-thumb" style={{
                   backgroundImage: `url(${thumb})`
                 }} />
                 <ul className="character-meta">
                   <li>
-                    <strong>Name:</strong> { name }
+                  <Highlight search={query} style={{ fontWeight: "bold" }}>{name}</Highlight>
                   </li>
                   <li>
-                    <strong>Company:</strong> { company }
+                    <strong>Company:</strong> 
+                    <Highlight search={query} style={{ fontWeight: "bold" }}>{company}</Highlight>
                   </li>
                   <li>
                     <strong>Species:</strong> { species }
